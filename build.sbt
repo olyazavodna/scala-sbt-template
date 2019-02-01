f6981d5941b0b96ad2fce0ab46466c177d74172c
Vlas Shatokhin <1974789+VlasShatokhin@users.noreply.github.com>
2019-01-31 22:46:10 -0600

    sample template initial

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "scala-sbt-template"
+
+version := "1.0"
+
+scalaVersion := "2.12.8"
+
+sourcesInBase := true
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % Test
---
+++ src/test/scala/edu/learn/scala/basic/OptionSpec.scala
@@ -0,0 +1,33 @@
+package edu.learn.scala.basic
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * [[https://danielwestheide.com/blog/2012/12/19/the-neophytes-guide-to-scala-part-5-the-option-type.html]]
+  */
+class OptionSpec extends FlatSpec with Matchers {
+
+  "Option" should "result in None when created with null" in {
+    val option = Option(null)
+
+    option shouldBe None
+  }
+
+  it should "result in Some value when created with an actual object" in {
+    val option = Option("test")
+
+    option shouldBe Some("test")
+  }
+
+  ignore should "execute foreach method if contains the value" in {
+    ???
+  }
+
+  ignore should "return a value on getOrElse if not empty" in {
+    ???
+  }
+
+  ignore should "return a fallback value on getOrElse if empty" in {
+    ???
+  }
+}
